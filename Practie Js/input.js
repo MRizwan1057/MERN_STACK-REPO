85dd96644e5e6e84fc1d0ee74793b8c02876f9a8
MRizwan1057 <96943208+MRizwan1057@users.noreply.github.com>
2022-09-23 21:39:20 +0500

    just practicing js

---
+++ Practie Js/input.js
@@ -0,0 +1,7 @@
+// First npm i prompt-sync, then use as below
+
+const prompt = require("prompt-sync")();
+let name = prompt("what's your name?");
+let age = prompt("how old are you??");
+
+console.log("Salaam   " + name + "\n \n " + age + "  Saal k ho na??" + "\n");
